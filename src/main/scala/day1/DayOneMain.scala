97094c5dcce759d87c7259e3463135a01b384316
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-09 12:06:32 +0100

    created function that prints provided argument

--- src/main/scala/day1/DayOneMain.scala
+++ src/main/scala/day1/DayOneMain.scala
@@ -2,10 +2,15 @@ package DayOneMain
 
 object DayOneMain {
 
+  def printInput(input: String) {
+    println(input)
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello World")
 
-
+    printInput(("input to print"))
   }
-
 }
+
+
