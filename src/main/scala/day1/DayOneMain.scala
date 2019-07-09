8140874dbb554208ae839f7c0edbe4dab460cd43
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-09 12:11:53 +0100

    added function that can take any input and print it to the console

--- src/main/scala/day1/DayOneMain.scala
+++ src/main/scala/day1/DayOneMain.scala
@@ -10,10 +10,20 @@ object DayOneMain {
     return "Returned String"
   }
 
+  def outputAnyInput(input: Any): Unit = {
+    println(input)
+  }
+
+
   def main(args: Array[String]): Unit = {
     println("Hello World")
 
     printInput(returnString())
+
+    outputAnyInput(1.1)
+    outputAnyInput("TestOutput")
+    outputAnyInput(1)
+    outputAnyInput(true)
   }
 }
 
