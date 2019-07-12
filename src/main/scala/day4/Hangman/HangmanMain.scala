d28a6fdab8f49fea22b29416bb9c6554c511f35a
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-12 14:36:32 +0100

    hangman is functional

--- src/main/scala/day4/Hangman/HangmanMain.scala
+++ src/main/scala/day4/Hangman/HangmanMain.scala
@@ -12,8 +12,35 @@ object HangmanMain {
     Source.fromFile("words-for-anagram.txt").getLines().toList.filter(item => !item.contains('\''))
   }
 
+  def getPlayerInput(): Char = {
+    var inputGotten = false
+    var input = ""
+    while(!inputGotten) {
+      println("\n\nplease enter a character")
+      input = scala.io.StdIn.readLine()
+      if(input.length == 1 && input.matches("[a-z]")) {
+        inputGotten = true
+        inputGotten
+      } else {
+        println("error, please enter a character a-z")
+      }
+    }
+    input.toCharArray()(0)
+  }
+
+  def searchForPlayerInput(curSolvedCharIndex: ListBuffer[Int], wordToSearch: Array[String], charToSearchFor: Char): Boolean = {
+    if(wordToSearch.contains(charToSearchFor.toString)) {
+      wordToSearch.zipWithIndex foreach { case(character, index) =>
+        if(character.toCharArray()(0) == charToSearchFor && !curSolvedCharIndex.contains(index)) curSolvedCharIndex += index
+      }
+      true
+    } else false
+  }
+
+
+
   def main(args: Array[String]): Unit = {
-    val playerLives = 11
+    var playerLives = 10
     val words = getWordOptions()
     val randomNumGen = scala.util.Random
     randomNumGen.setSeed(Calendar.getInstance.get(Calendar.MILLISECOND))
@@ -22,12 +49,103 @@ object HangmanMain {
     val solvedCharIndex: ListBuffer[Int] = ListBuffer()
 
     while (!solved && playerLives > 0) {
-      selectedWord.split("").zipWithIndex foreach { case(el, i) =>
-          print(if(solvedCharIndex.contains(i)) i else " _ ")
-        }
-      solved = true
+      val charArray = selectedWord.split("")
+      charArray.zipWithIndex foreach { case(character, index) =>
+          print(if(solvedCharIndex.contains(index)) s" $character " else " _ ")
+      }
+      if (!searchForPlayerInput(solvedCharIndex, charArray, getPlayerInput())) playerLives -= 1
+
+      drawHangman(playerLives)
+
+      if(solvedCharIndex.length == selectedWord.length) {
+        println("Congratulations!!")
+        solved = true
+      }
     }
+
+    println(s"The Word was: $selectedWord")
   }
 
+  def drawHangman(playerLives: Int): Unit = {
+    playerLives match {
+      case 9 => println("-------------")
+      case 8 => {
+        println("|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|")
+        println("-------------")
+      }
+      case 7 => {
+        println("-----------")
+        println("|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|")
+        println("---------------------------")
+      }
+      case 6 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|\n|\n|\n|\n|\n|\n|\n|")
+        println("---------------------------")
+      }
+      case 5 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|         0\n" +
+          "|\n|\n|\n|\n|\n|\n|")
+        println("---------------------------")
+      }
+      case 4 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|         0\n" +
+          "|         |\n" +
+          "|         |\n" +
+          "|\n|\n|\n|")
+        println("---------------------------")
+      }
+      case 3 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|         0\n" +
+          "|        /|\n" +
+          "|         |\n" +
+          "|\n|\n|\n|")
+        println("---------------------------")
+      }
+      case 2 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|         0\n" +
+          "|        /|\\\n" +
+          "|         |\n" +
+          "|\n|\n|\n|")
+        println("---------------------------")
+      }
+      case 1 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|         0\n" +
+          "|        /|\\\n" +
+          "|         |\n" +
+          "|        /\n" +
+          "|\n|")
+        println("---------------------------")
+      }case 0 => {
+        println("-----------")
+        println("|         |\n"+
+          "|         |\n" +
+          "|         0\n" +
+          "|        /|\\\n" +
+          "|         |\n" +
+          "|        / \\\n" +
+          "|\n|")
+        println("---------------------------")
+      }
 
+      case _ =>
+    }
+  }
 }
