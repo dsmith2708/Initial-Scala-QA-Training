f355d069484dfd0c1b483db0960da1ee16b3b8e2
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-12 11:11:33 +0100

    attempted to get timer working, it does not

--- src/test/scala/DayOneTest.scala
+++ src/test/scala/DayOneTest.scala
@@ -22,4 +22,44 @@ class DayOneTest extends FlatSpec with Matchers {
 
   }
 
+  "returnSumOrMultiplicationOfIntsUnlessZero" should "return multiplication of two ints if both are positive," +
+    "and boolean is false" in {
+    val expectedResult = 25
+    val inputValue = 5
+    val inputBool = false
+    assertResult(expectedResult)(DayOneMain.returnSumOrMultiplicationOfIntsUnlessZero(inputValue, inputValue, inputBool))
+  }
+
+  "returnSumOrMultiplicationOfIntsUnlessZero" should "return addition of two ints if both are positive," +
+    "and boolean is false" in {
+    val expectedResult = 10
+    val inputValue = 5
+    val inputBool = true
+    assertResult(expectedResult)(DayOneMain.returnSumOrMultiplicationOfIntsUnlessZero(inputValue, inputValue, inputBool))
+  }
+
+  "returnSumOrMultiplicationOfIntsUnlessZero" should "return the first value if a 0 is provided as the second value" +
+    "and boolean is false" in {
+    val expectedResult = 5
+    val inputValueOne = 5
+    val inputValueTwo = 0
+    val inputBool = true
+    assertResult(expectedResult)(DayOneMain.returnSumOrMultiplicationOfIntsUnlessZero(inputValueOne, inputValueTwo, inputBool))
+  }
+
+  "returnSumOrMultiplicationOfIntsUnlessZero" should "return the second value if a 0 is provided as the first value" +
+    "and boolean is false" in {
+    val expectedResult = 5
+    val inputValueOne = 0
+    val inputValueTwo = 5
+    val inputBool = true
+    assertResult(expectedResult)(DayOneMain.returnSumOrMultiplicationOfIntsUnlessZero(inputValueOne, inputValueTwo, inputBool))
+  }
+
+  "blackJackCheck" should "output the closest value to 21 of two numbers below 21" in {
+    val inputValueOne = 15
+    val inputValueTwo = 20
+    DayOneMain.blackJackCheck(inputValueOne, inputValueTwo) should be (inputValueTwo)
+  }
+
 }
